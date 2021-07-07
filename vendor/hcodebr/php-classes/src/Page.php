157f6d8fb95a5b84638b4e2082d06bc6e0572a38
Danilo Henrique <daniloohenriquee@gmail.com>
2021-07-07 19:45:45 -0300

    correcao da pasta do arquivo Page.php

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -13,13 +13,13 @@ class Page {
     ];
 
     public function __construct($opts = array()) {
-       
+
         $this->$options = array_merge($this->defaults, $opts);
 
         $config = array(
             "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
             "cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
-            "debug"         => false // set to false to improve the speed
+            "debug"         => false
            );
 
         Tpl::configure( $config );
